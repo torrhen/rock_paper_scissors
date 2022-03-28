6db70619fa46bae18407ea27c568ff0e2e427192
torrhen <toby.coleman96@outlook.com>
2022-03-28 20:22:22 +0100

    Modifies sobel operator
    Increases the size of the kernel for guassian blur and sobel edge detection of each processed image.

--- src/images.py
+++ src/images.py
@@ -5,8 +5,8 @@ import glob
 def apply_edge_detection(image):
     # low_threshold = 255/3
     # high_threshold = 255
-    image = cv.GaussianBlur(image, (3,3), 0)
-    return cv.Sobel(src=image, ddepth=-1, dx=1, dy=1, ksize=5)
+    image = cv.GaussianBlur(image, (7,7), 0)
+    return cv.Sobel(src=image, ddepth=-1, dx=1, dy=1, ksize=7)
     # return cv.Canny(image, low_threshold, high_threshold)
 
 def resize_and_normalize(image):
